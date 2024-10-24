15b8fbd0c89433c9f0d5abbd6fc23cf2abc995da
Matheus Nicacio <110930694+matheussnicacio@users.noreply.github.com>
2024-10-24 17:43:04 -0300

    carrosel

--- js/products.js
+++ js/products.js
@@ -1,7 +1,13 @@
+
+
 const products = [
-  { id: 1, name: "Produto 1", description: "Descrição do Produto 1", price: 100.00 },
-  { id: 2, name: "Produto 2", description: "Descrição do Produto 2", price: 150.00 },
-  { id: 3, name: "Produto 3", description: "Descrição do Produto 3", price: 200.00 },
+  { id: 1, name: "Produto 1", description: "Descrição do Produto 1", price: 100.00, image: "../images/camisachadrez1-removebg-preview.png" },
+  { id: 2, name: "Produto 2", description: "Descrição do Produto 2", price: 150.00, image: "../images/logo.png" },
+  { id: 3, name: "Produto 3", description: "Descrição do Produto 3", price: 200.00, image: "../images/logo.png" },  
+  { id: 1, name: "Produto 1", description: "Descrição do Produto 1", price: 100.00, image: "../images/camisachadrez1-removebg-preview.png" },
+  { id: 2, name: "Produto 2", description: "Descrição do Produto 2", price: 150.00, image: "../images/logo.png" },
+  { id: 3, name: "Produto 3", description: "Descrição do Produto 3", price: 200.00, image: "../images/logo.png" },
+  
 ];
 
 function loadProducts() {
@@ -12,6 +18,7 @@ function loadProducts() {
               <div class="card mb-4">
                   <div class="card-body">
                       <h5 class="card-title">${product.name}</h5>
+                    <img src="${product.image}" class="img-fluid"/>
                       <p class="card-text">${product.description}</p>
                       <a href="product-details.html?id=${product.id}" class="btn btn-primary">Ver detalhes</a>
                   </div>
