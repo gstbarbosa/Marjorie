661f4aa6c6b5f821d37b2bbd22e57fa61c3ab7e3
Matheus Nicacio <110930694+matheussnicacio@users.noreply.github.com>
2024-10-26 15:31:11 -0300

    Tela detalhes III

--- js/productDetails.js
+++ js/productDetails.js
@@ -23,14 +23,16 @@ function loadProductDetails() {
     const product = products.find(p => p.id == productId);
     if (product) {
         document.getElementById('product-details').innerHTML = `
-            <h2>${product.name}</h2>
-            <p>${product.description}</p>
-            <p><strong>Preço:</strong> R$${product.price.toFixed(2)}</p>
+        <h2>${product.name}</h2>
+        <p>${product.description}</p>
+        <p><strong>Preço:</strong> R$${product.price.toFixed(2)}</p>
+        <div class="image-container">
             <img src="${product.image}" class="img-fluid main-image" alt="${product.name}"/>
             <div class="secondary-images">
                 ${product.secondaryImages.map(img => `<img src="${img}" class="img-thumbnail" alt="Imagem secundária"/>`).join('')}
             </div>
-        `;
+        </div>
+    `;
     } else {
         document.getElementById('product-details').innerHTML = '<p>Produto não encontrado!</p>';
     }
--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 
 
 const products = [
-  { id: 1, name: "Produto 1", description: "Descrição do Produto 1", price: 100.00, image: "../images/camisachadrez1-removebg-preview.png", secondaryImages: ["../images/camisachadrez1-removebg-preview.png", "../imagesslide/Slide 1"]},
+  { id: 1, name: "Produto 1", description: "Descrição do Produto 1", price: 100.00, image: "../images/camisachadrez1-removebg-preview.png", secondaryImages: ["../images/camisachadrez1-removebg-preview.png", "../images/camisachadrez1-removebg-preview.png"]},
   { id: 2, name: "Produto 2", description: "Descrição do Produto 2", price: 150.00, image: "../images/logo.png", secondaryImages: ["../images/camisachadrez1-removebg-preview.png", "../imagesslide/Slide 1"] },
   { id: 3, name: "Produto 3", description: "Descrição do Produto 3", price: 200.00, image: "../images/logo.png", secondaryImages: ["../images/camisachadrez1-removebg-preview.png", "../imagesslide/Slide 1"] },  
   { id: 4, name: "Produto 1", description: "Descrição do Produto 1", price: 100.00, image: "../images/camisachadrez1-removebg-preview.png", secondaryImages: ["../images/camisachadrez1-removebg-preview.png", "../imagesslide/Slide 1"] },
