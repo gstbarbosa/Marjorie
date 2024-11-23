734d4421be9ad84ad1f629f3032e5f0c2d6fb888
GustavoABarbosa <gustavo.barbosa@catolicasc.edu.br>
2024-11-23 18:01:12 -0300

    Ajustes gerais

--- js/productDetails.js
+++ js/productDetails.js
@@ -5,36 +5,38 @@ function loadProductDetails() {
     const product = products.find(p => p.id == productId);
     if (product) {
         document.getElementById('product-details').innerHTML = `
-            <h2>${product.name}</h2>
-            <img src="${product.image}" class="img-fluid"/>
-            <p>${product.description}</p>
-            <p class="price"><strong></strong>R$${product.price.toFixed(2)}</p>
-        `;
-    } else {
-        document.getElementById('product-details').innerHTML = '<p>Produto não encontrado!</p>';
-    }
-}
-
-document.addEventListener('DOMContentLoaded', loadProductDetails);
-
-
-
-function loadProductDetails() {
-    const product = products.find(p => p.id == productId);
-    if (product) {
-        document.getElementById('product-details').innerHTML = `
       
     
-        <div class="image-container mt-3">
-            <img src="${product.image}" class="img-fluid main-image" alt="${product.name}"/>
+        <div class="image-container">
+            <img src="${product.image}" class="main-image" alt="${product.name}"/>
             <div class="secondary-images">
                 ${product.secondaryImages.map(img => `<img src="${img}" class="img-thumbnail" alt="Imagem secundária"/>`).join('')}
             </div>
         </div>
-        <div class="container mt-3">
-        <h2>${product.name}</h2>
-        <p>${product.description}</p>
-        <p class="price"><strong></strong> R$${product.price.toFixed(2)}</p>
+        <div class="container" style="padding: 0">
+            <h2>${product.name}</h2>
+            <div class="values">
+                <p class="price">R$${product.price.toFixed(2)}</p>
+                <p class="price-sm">a vista</p>
+            </div>
+            <p class="price-sm" style="margin-top: -10px">ou R$${product.fullPrice.toFixed(2)} em até 4x</p>
+            <div class="description-card">
+                <p style="margin: 0">${product.description}</p>
+            </div>
+            <div class="sizeAndCep">
+                <div id="size-selector" class="size-selector">
+                    <div class="size-option" id="size-P" onclick="selectSize('P')">P</div>
+                    <div class="size-option" id="size-M" onclick="selectSize('M')">M</div>
+                    <div class="size-option" id="size-G" onclick="selectSize('G')">G</div>
+                </div>
+                <div class="cep-card">
+                    <p style="margin: 0; font-weight: bold">CEP</p>
+                    <input style="width: 100%">
+                    <button class="btn btn-secundary" type="button" style="border: none">
+                        <span>CALCULAR</span>
+                    </button>                    
+                </div>
+            </div>
         </div>
     `;
     } else {
--- js/suggestions.js
+++ js/suggestions.js
@@ -4,18 +4,22 @@ products.forEach(product => {
     const card = document.createElement('div');
     card.classList.add('product-card'); 
     card.innerHTML = `
-    <div>
-    <a href="product-suggestions.html?id=${product.id}" style="text-decoration:none";>
-
+    <div >
+        <a href="product-details.html?id=${product.id}" style="text-decoration:none";>
         <div class="card mb-4">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-              <img src="${product.image}" class="img-fluid"/>
-              <p class="price"><strong></strong>R$${product.price.toFixed(2)}</p>
+          <div class="card-body" style="margin: auto">
+            <div class="product-image">
+              <img src="${product.image}"/>
             </div>
-
+            <h5 class="card-title">${product.name}</h5>
+            <div class="card-values">
+              <p class="card-price">R$${product.price.toFixed(2)}</p>
+              <p class="card-price-sm">a vista</p>
+            </div>
+            <p class="card-price-sm" style="margin-left:10px">ou R$${product.fullPrice.toFixed(2)} em até 4x</p>
+          </div>
         </div>
-        </a>
+      </a>
     </div>
 
     `;
